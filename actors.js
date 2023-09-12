c5303de39ebf8d7bcade686bc338034ad8970fb5
AndresBriend98 <briendandres@gmail.com>
2023-09-12 16:27:12 -0300

    final

--- actors.js
+++ actors.js
@@ -1,41 +1,41 @@
 const comedy = require("comedy");
-const { IdaActor, VueltaActor } = require("./scrapping"); // Asegúrate de usar la ruta correcta
+const { IdaActor, VueltaActor } = require("./scrapping"); // Importa las funciones de las clases
 
-const actors = comedy();
+const actors = comedy(); // Instancia del sistema de actores
 
 async function main() {
   try {
-    const rootActor = await actors.rootActor();
+    const rootActor = await actors.rootActor(); // Creamos el actor raíz del sistema de actores
 
     // Actor local para Ida
-    const idaActor = await rootActor.createChild(IdaActor);
-    const idaData = await idaActor.sendAndReceive("scrapeIda");
-    console.log("Resultados de Ida (local):", idaData);
+    const idaActor = await rootActor.createChild(IdaActor); // Creamos un actor local
+    const idaData = await idaActor.sendAndReceive("scrapeIda"); // Envía un mensaje al actor y recibe una respuesta
+    console.log("Resultados de Ida (local):", idaData); // Imprime los resultados
 
     // Actor local para Vuelta
-    const vueltaActor = await rootActor.createChild(VueltaActor);
-    const vueltaData = await vueltaActor.sendAndReceive("scrapeVuelta");
-    console.log("Resultados de Vuelta (local):", vueltaData);
+    const vueltaActor = await rootActor.createChild(VueltaActor); // Crea un actor local
+    const vueltaData = await vueltaActor.sendAndReceive("scrapeVuelta"); // Envía un mensaje al actor y recibe una respuesta
+    console.log("Resultados de Vuelta (local):", vueltaData); // Imprime los resultados
 
     // Actor remoto para Ida
     const idaActorRemoto = await rootActor.createChild(IdaActor, {
       mode: "remote",
-      host: "192.168.1.26", // Reemplaza con la dirección IP correcta del actor remoto
+      host: "192.168.1.26",
     });
-    const idaDataRemoto = await idaActorRemoto.sendAndReceive("scrapeIda");
-    console.log("Resultados de Ida (remoto):", idaDataRemoto);
+    const idaDataRemoto = await idaActorRemoto.sendAndReceive("scrapeIda"); // Envía un mensaje al actor remoto y recibe una respuesta
+    console.log("Resultados de Ida (remoto):", idaDataRemoto); // Imprime los resultados
 
     // Actor remoto para Vuelta
     const vueltaActorRemoto = await rootActor.createChild(VueltaActor, {
       mode: "remote",
-      host: "192.168.1.26", // Reemplaza con la dirección IP correcta del actor remoto
+      host: "192.168.1.26",
     });
-    const vueltaDataRemoto = await vueltaActorRemoto.sendAndReceive("scrapeVuelta");
-    console.log("Resultados de Vuelta (remoto):", vueltaDataRemoto);
+    const vueltaDataRemoto = await vueltaActorRemoto.sendAndReceive("scrapeVuelta"); // Envía un mensaje al actor remoto y recibe una respuesta
+    console.log("Resultados de Vuelta (remoto):", vueltaDataRemoto); // Imprime los resultados
   } catch (err) {
-    console.error(err);
+    console.error(err); 
   } finally {
-    actors.destroy();
+    actors.destroy(); // Destruye el sistema de actores al finalizar
   }
 }
 
--- scrapping.js
+++ scrapping.js
@@ -3,6 +3,7 @@ const request = require('request-promise');
 
 class IdaActor {
   async scrapeIda() {
+    // Llama a la función getTresFechasPrecios con la URL de Ida y retorna los resultados
     const resultadosIda = await this.getTresFechasPrecios("https://flybondi.com/ar/search/dates?adults=1&children=0&fromCityCode=BUE&infants=0&toCityCode=MDZ");
     return resultadosIda;
   }
@@ -11,6 +12,7 @@ class IdaActor {
     const resultados = [];
 
     try {
+      // Realiza una solicitud HTTP a la URL y carga la página en cheerio
       const $ = await request({
         uri: url,
         transform: (body) => cheerio.load(body),
@@ -18,17 +20,19 @@ class IdaActor {
 
       const precios = [];
 
+      // Busca los elementos HTML correspondientes a los precios y las fechas
       $('.dt-row-group .jsx-468550366.fare-day').each((i, precio) => {
         const precioTexto = $(precio).text();
         const precioNumero = parseFloat(precioTexto.replace('$', '').replace(',', ''));
 
         if (!isNaN(precioNumero)) {
-          precios.push({ date: i + 1, price: precioNumero }); // Corregimos el índice para que el mes comience en el día 1
+          precios.push({ date: i + 1, price: precioNumero }); // Corrige el índice para que el mes comience en el día 1
         }
       });
 
       precios.sort((a, b) => a.price - b.price);
 
+      // Agrega los tres precios más bajos al arreglo de resultados
       resultados.push(precios[0]);
       resultados.push(precios[1]);
       resultados.push(precios[2]);
@@ -41,8 +45,10 @@ class IdaActor {
   }
 }
 
+
 class VueltaActor {
   async scrapeVuelta() {
+    // Llama a la función getTresFechasPrecios con la URL de Vuelta y retorna los resultados
     const resultadosVuelta = await this.getTresFechasPrecios("https://flybondi.com/ar/search/dates?adults=1&children=0&currency=ARS&fromCityCode=MDZ&infants=0&toCityCode=BUE&utm_origin=search_bar");
     return resultadosVuelta;
   }
@@ -51,6 +57,7 @@ class VueltaActor {
     const resultados = [];
 
     try {
+      // Realiza una solicitud HTTP a la URL y carga la página en cheerio
       const $ = await request({
         uri: url,
         transform: (body) => cheerio.load(body),
@@ -58,17 +65,19 @@ class VueltaActor {
 
       const precios = [];
 
+      // Busca los elementos HTML correspondientes a los precios y las fechas
       $('.dt-row-group .jsx-468550366.fare-day').each((i, precio) => {
         const precioTexto = $(precio).text();
         const precioNumero = parseFloat(precioTexto.replace('$', '').replace(',', ''));
 
         if (!isNaN(precioNumero)) {
-          precios.push({ date: i + 1, price: precioNumero }); // Corregimos el índice para que el mes comience en el día 1
+          precios.push({ date: i + 1, price: precioNumero }); // Corrige el índice para que el mes comience en el día 1
         }
       });
 
       precios.sort((a, b) => a.price - b.price);
 
+      // Agrega los tres precios más bajos al arreglo de resultados
       resultados.push(precios[0]);
       resultados.push(precios[1]);
       resultados.push(precios[2]);
@@ -81,6 +90,7 @@ class VueltaActor {
   }
 }
 
+// Exportamos las clases IdaActor y VueltaActor
 module.exports = {
   IdaActor,
   VueltaActor,
