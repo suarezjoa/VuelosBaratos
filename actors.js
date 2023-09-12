e2cc2d9a24bfef521ea8201340ec7f5f3ede665f
AndresBriend98 <briendandres@gmail.com>
2023-09-12 17:44:26 -0300

    finalizado

--- actors.js
+++ actors.js
@@ -1,41 +1,41 @@
 const comedy = require("comedy");
 const { IdaActor, VueltaActor } = require("./scrapping"); // Importa las funciones de las clases
 
-const actors = comedy(); // Instancia del sistema de actores
+const actors = comedy();// Creamos una instancia del sistema de actores
 
 async function main() {
   try {
     const rootActor = await actors.rootActor(); // Creamos el actor raíz del sistema de actores
 
     // Actor local para Ida
-    const idaActor = await rootActor.createChild(IdaActor); // Creamos un actor local
+    const idaActor = await rootActor.createChild(IdaActor); // Crea un actor local
     const idaData = await idaActor.sendAndReceive("scrapeIda"); // Envía un mensaje al actor y recibe una respuesta
-    console.log("Resultados de Ida (local):", idaData); // Imprime los resultados
+    console.log("Resultados de Ida (local):", idaData);// Imprime los resultados
 
     // Actor local para Vuelta
     const vueltaActor = await rootActor.createChild(VueltaActor); // Crea un actor local
-    const vueltaData = await vueltaActor.sendAndReceive("scrapeVuelta"); // Envía un mensaje al actor y recibe una respuesta
-    console.log("Resultados de Vuelta (local):", vueltaData); // Imprime los resultados
+    const vueltaData = await vueltaActor.sendAndReceive("scrapeVuelta");// Envía un mensaje al actor y recibe una respuesta
+    console.log("Resultados de Vuelta (local):", vueltaData);// Imprime los resultados
 
-    // Crear un actor remoto para Ida
-    const idaActorRemoto = await rootActor.createChild(RemoteActor, {
+    // Actor remoto para Ida
+    const idaActorRemoto = await rootActor.createChild(IdaActor, {
       mode: "remote",
       host: "192.168.1.26",
     });
-    const idaDataRemoto = await idaActorRemoto.sendAndReceive("scrapeIda"); // Envía un mensaje al actor remoto y recibe una respuesta
-    console.log("Resultados de Ida (remoto):", idaDataRemoto); // Imprime los resultados
+    const idaDataRemoto = await idaActorRemoto.sendAndReceive("scrapeIda");
+    console.log("Resultados de Ida (remoto):", idaDataRemoto);
 
-    // Crear un actor remoto para Vuelta
+    // Actor remoto para Vuelta
     const vueltaActorRemoto = await rootActor.createChild(VueltaActor, {
       mode: "remote",
       host: "192.168.1.26",
     });
-    const vueltaDataRemoto = await vueltaActorRemoto.sendAndReceive("scrapeVuelta"); // Envía un mensaje al actor remoto y recibe una respuesta
-    console.log("Resultados de Vuelta (remoto):", vueltaDataRemoto); // Imprime los resultados
+    const vueltaDataRemoto = await vueltaActorRemoto.sendAndReceive("scrapeVuelta");
+    console.log("Resultados de Vuelta (remoto):", vueltaDataRemoto);
   } catch (err) {
-    console.error(err); 
+    console.error(err);
   } finally {
-    actors.destroy(); // Destruye el sistema de actores al finalizar
+    actors.destroy();
   }
 }
 
